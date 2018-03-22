b092ad2dafe5aab7f30f518984cca73293a8c39a
lei_mendenilla <https://lei_mendenilla@bitbucket.org>
2018-03-22 13:42:59 +0800

    change modal structure

---
+++ resources/views/components/modal/edit.blade.php
@@ -0,0 +1,11 @@
+<div id="edit_modal" class="modal modal-fixed-footer">
+  <div class="modal-content">
+    <h4>Edit</h4>
+    <div class="modal-content-container">
+    </div>
+  </div>
+  <div class="modal-footer">
+      <a class="modal-action modal-close waves-effect waves-green btn-flat ">Cancel</a>
+    <a class="modal-action waves-effect btn-flat save-btn" >Save</a>
+  </div>
+</div>
